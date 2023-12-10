b1f2079ee506b2cdf636fd1c3c77d91faf6b5bc1
Mateo Guzmán <info@mateoguzman.net>
2023-12-10 20:17:44 +0100

    feat(ios): bootstrapping initial method structure

--- ios/ZendeskUnified.mm
+++ ios/ZendeskUnified.mm
@@ -6,6 +6,9 @@ RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
 
+RCT_EXTERN_METHOD(healthCheck:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
+
 + (BOOL)requiresMainQueueSetup
 {
   return NO;
--- ios/ZendeskUnified.swift
+++ ios/ZendeskUnified.swift
@@ -5,4 +5,27 @@ class ZendeskUnified: NSObject {
   func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
     resolve(a*b)
   }
+
+  @objc(healthCheck:withRejecter:)
+  func healthCheck(resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve("Module compiling and working")
+  }
+
+    //   Function("healthCheck") {
+    //   return "Module compiling and working"
+    // }
+
+    // AsyncFunction("initialize") { (
+    //   appId: String, 
+    //   clientId: String,
+    //   zendeskUrl: String,
+    //   accountKey: String?
+    // ) in
+    //   initializeZendesk(appId: appId, clientId: clientId, zendeskUrl: zendeskUrl)
+
+    //   if let accountKey = accountKey {
+    //     initializeChat(accountKey: accountKey)
+    //   }
+    // }
+  
 }
